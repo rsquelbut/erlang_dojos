638109b38c8c7d95e63a561dd6cb79b67c2886a6
rsquelbut <java4job@gmail.com>
2015-12-07 22:28:08 +0100

    [fizzbuzz] réutilise correctement les macros

--- src/fizzbuzz.erl
+++ src/fizzbuzz.erl
@@ -13,17 +13,17 @@
 -export([fizzbuzz/1, fizzbuzzz/1]).
 
 -define(is_modulo(Input, Divider), Input rem Divider == 0).
--define(is_modulo3(Input), Input rem 3 == 0).
--define(is_modulo5(Input), is_modulo(Input, 5)).
--define(is_modulo15(Input), is_modulo(Input, 15)).
+-define(is_modulo3(Input), ?is_modulo(Input, 3)).
+-define(is_modulo5(Input), ?is_modulo(Input, 5)).
+-define(is_modulo15(Input), ?is_modulo(Input, 15)).
 
 fizzbuzz(0) ->
   0;
-fizzbuzz(X) when ?is_modulo(X, 15) ->
+fizzbuzz(X) when ?is_modulo15(X) ->
   fizzbuzz;
 fizzbuzz(X) when ?is_modulo3(X) ->
   fizz;
-fizzbuzz(X) when ?is_modulo(X, 5) ->
+fizzbuzz(X) when ?is_modulo5(X) ->
   buzz;
 fizzbuzz(X) ->
   X.
